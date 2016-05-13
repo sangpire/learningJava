6fc43d82c6d4a31fb85c6406347c247e42dee8c4
Stewie.byun <stewie.byun@daumkakao.com>
2016-05-13 09:41:42 +0900

    Add MathTest.

---
+++ src/test/java/learning/java/lang/MathTest.java
@@ -0,0 +1,27 @@
+package learning.java.lang;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by sangpire on 2016. 5. 13..
+ */
+public class MathTest {
+
+    @Test
+    public void testCeil() {
+        assertThat((5/2), is(2));
+        assertThat((5/2), is(instanceOf(Integer.class)));
+        assertThat(((double) 5/2), is(2.5));
+        assertThat(((double) 5/2), is(instanceOf(Double.class)));
+
+        assertThat((int) Math.ceil((double) 5/2), is(3));
+    }
+
+    @Test
+    public void testFloor() {
+        assertThat((int) Math.floor((double) 5/2), is(2));
+    }
+}
