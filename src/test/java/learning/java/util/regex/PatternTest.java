0b035ee28ef28a78f03c2e79f7c69b5ac1692905
BYUN Sangpil <sangpire@gmail.com>
2016-05-25 12:14:31 +0900

    - util.regex.Pattern 테스트 클래스 추가.

---
+++ src/test/java/learning/java/util/regex/PatternTest.java
@@ -0,0 +1,26 @@
+package learning.java.util.regex;
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class PatternTest {
+
+    public static final String REGEX = "[PD]{1}-\\d+";
+
+    @Test
+    public void testUseage() {
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher("P-4563");
+        assertThat(matcher.matches(), is(true));
+
+        assertThat(Pattern.matches(REGEX, "D-456"), is(true));
+        assertThat(Pattern.matches(REGEX, "D--456"), is(false));
+
+        assertThat(Pattern.matches(REGEX, "D-45D"), is(false));
+    }
+}
