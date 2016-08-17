33562c7c3e9a921e59450c7834dc2ad0e05c6da3
BYUN Sangpil <sangpire@gmail.com>
2016-08-17 12:04:14 +0900

    플롯/플롯 패턴 추가.

--- src/test/java/learning/java/util/regex/PatternTest.java
+++ src/test/java/learning/java/util/regex/PatternTest.java
@@ -10,26 +10,54 @@ import static org.junit.Assert.assertThat;
 
 public class PatternTest {
 
-    public static final String REGEX = "([PD]{1})-(\\d+)";
+    public static final String REGEX_1 = "([PD]{1})-(\\d+)";
+    public static final String REGEX_2 = "([0-9.]*)\\/([0-9.]+)";
 
     @Test
     public void testUseage() {
-        Pattern pattern = Pattern.compile(REGEX);
+        Pattern pattern = Pattern.compile(REGEX_1);
         Matcher matcher = pattern.matcher("P-4563");
         assertThat(matcher.matches(), is(true));
 
-        assertThat(Pattern.matches(REGEX, "D-456"), is(true));
-        assertThat(Pattern.matches(REGEX, "D--456"), is(false));
+        assertThat(Pattern.matches(REGEX_1, "D-456"), is(true));
+        assertThat(Pattern.matches(REGEX_1, "D--456"), is(false));
 
-        assertThat(Pattern.matches(REGEX, "D-45D"), is(false));
+        assertThat(Pattern.matches(REGEX_1, "D-45D"), is(false));
     }
 
     @Test
     public void testMatcher() {
-        Pattern pattern = Pattern.compile(REGEX);
+        Pattern pattern = Pattern.compile(REGEX_1);
         Matcher matcher = pattern.matcher("P-4563");
         assertThat(matcher.matches(), is(true));
         assertThat(matcher.group(1), is("P"));
         assertThat(matcher.group(2), is("4563"));
+    }
+
+    @Test
+    public void testMatcherForRegex2_1() {
+        Pattern pattern = Pattern.compile(REGEX_2);
+        Matcher matcher = pattern.matcher("3.4/4.5");
+        assertThat(matcher.matches(), is(true));
+        assertThat(matcher.group(1), is("3.4"));
+        assertThat(matcher.group(2), is("4.5"));
+    }
+
+    @Test
+    public void testMatcherForRegex2_2() {
+        Pattern pattern = Pattern.compile(REGEX_2);
+        Matcher matcher = pattern.matcher("3/4.5");
+        assertThat(matcher.matches(), is(true));
+        assertThat(matcher.group(1), is("3"));
+        assertThat(matcher.group(2), is("4.5"));
+    }
+
+    @Test
+    public void testMatcherForRegex2_3() {
+        Pattern pattern = Pattern.compile(REGEX_2);
+        Matcher matcher = pattern.matcher("3/5");
+        assertThat(matcher.matches(), is(true));
+        assertThat(matcher.group(1), is("3"));
+        assertThat(matcher.group(2), is("5"));
     }
 }
